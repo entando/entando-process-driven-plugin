2606f3d4c7a4e6f521f5a7acf8810bfa56274b87
Filipe Leandro <filipeleandro87@gmail.com>
2019-08-30 15:55:53 -0300

    cleaning up

--- src/main/java/org/entando/plugins/pda/dto/connection/ConfigServiceConnectionsRequest.java
+++
@@ -1,9 +0,0 @@
-package org.entando.plugins.pda.dto.connection;
-
-import org.entando.web.response.SimpleRestResponse;
-
-import java.util.Map;
-
-public class ConfigServiceConnectionsUpdateRequest extends SimpleRestResponse<Map<String,ConnectionDto>> {
-
-}
