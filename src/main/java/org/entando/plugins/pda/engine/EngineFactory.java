f56f0488bad27f1e2116245f457c0760e1860b1f
Filipe Leandro <filipeleandro87@gmail.com>
2019-09-16 11:39:32 -0300

    PDA10 decoupling EngineFactory from each specific Engine

--- src/main/java/org/entando/plugins/pda/engine/EngineFactory.java
+++ src/main/java/org/entando/plugins/pda/engine/EngineFactory.java
@@ -1,34 +1,33 @@
 package org.entando.plugins.pda.engine;
 
 import org.entando.plugins.pda.core.engine.Engine;
-import org.entando.plugins.pda.core.engine.FakeEngine;
 import org.entando.plugins.pda.core.exception.EngineNotFoundException;
-import org.entando.plugins.pda.pam.engine.KieEngine;
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.List;
 
 @Component
-public class EngineFactory implements ApplicationContextAware  {
-    private Map<String, Engine> availableEngines = new HashMap<>();
+public class EngineFactory {
 
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        availableEngines.put(FakeEngine.TYPE, applicationContext.getBean(FakeEngine.class));
-        availableEngines.put(KieEngine.TYPE, applicationContext.getBean(KieEngine.class));
-    }
+    @Autowired
+    private List<Engine> availableEngines;
 
     public Engine getEngine(String type) {
-        return Optional.ofNullable(availableEngines.get(type))
-                .orElseThrow(EngineNotFoundException::new);
+        for (Engine engine : availableEngines) {
+            if(engine.getType().equals(type)) {
+                return engine;
+            }
+        }
+
+        throw new EngineNotFoundException();
     }
 
     public void validateEngine(String type) {
         getEngine(type);
+    }
+
+    public void setAvailableEngines(List<Engine> availableEngines) {
+        this.availableEngines = availableEngines;
     }
 }
--- src/test/java/org/entando/plugins/pda/engine/EngineFactoryUnitTest.java
+++ src/test/java/org/entando/plugins/pda/engine/EngineFactoryUnitTest.java
@@ -1,10 +1,14 @@
 package org.entando.plugins.pda.engine;
 
+import org.entando.plugins.pda.core.engine.Engine;
 import org.entando.plugins.pda.core.engine.FakeEngine;
 import org.entando.plugins.pda.pam.engine.KieEngine;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.eq;
@@ -19,14 +23,11 @@ public class EngineFactoryUnitTest {
     public void setup() {
         applicationContext = mock(ApplicationContext.class);
 
-        when(applicationContext.getBean(eq(KieEngine.class)))
-            .thenReturn(new KieEngine(null));
-
-        when(applicationContext.getBean(eq(FakeEngine.class)))
-                .thenReturn(new FakeEngine(null));
+        List<Engine> availableEngines = new ArrayList<>();
+        availableEngines.add(new FakeEngine(null));
 
         engineFactory = new EngineFactory();
-        engineFactory.setApplicationContext(applicationContext);
+        engineFactory.setAvailableEngines(availableEngines);
     }
 
     @Test
@@ -34,12 +35,5 @@ public class EngineFactoryUnitTest {
         verify(applicationContext).getBean(eq(FakeEngine.class));
 
         assertThat(engineFactory.getEngine(FakeEngine.TYPE)).isInstanceOf(FakeEngine.class);
-    }
-
-    @Test
-    public void testKieEngine() {
-        verify(applicationContext).getBean(eq(KieEngine.class));
-
-        assertThat(engineFactory.getEngine(KieEngine.TYPE)).isInstanceOf(KieEngine.class);
     }
 }
