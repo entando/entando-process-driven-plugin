d874c31e395663ac2c8ae48f864078f31d1bfac9
Filipe Leandro <filipeleandro87@gmail.com>
2019-09-17 10:26:46 -0300

    PDA10 fixing EngineFactory unit tests

--- src/test/java/org/entando/plugins/pda/engine/EngineFactoryUnitTest.java
+++ src/test/java/org/entando/plugins/pda/engine/EngineFactoryUnitTest.java
@@ -2,27 +2,19 @@ package org.entando.plugins.pda.engine;
 
 import org.entando.plugins.pda.core.engine.Engine;
 import org.entando.plugins.pda.core.engine.FakeEngine;
-import org.entando.plugins.pda.pam.engine.KieEngine;
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.*;
 
 public class EngineFactoryUnitTest {
-
-    private ApplicationContext applicationContext;
     private EngineFactory engineFactory;
 
     @Before
     public void setup() {
-        applicationContext = mock(ApplicationContext.class);
-
         List<Engine> availableEngines = new ArrayList<>();
         availableEngines.add(new FakeEngine(null));
 
@@ -32,8 +24,6 @@ public class EngineFactoryUnitTest {
 
     @Test
     public void testFakeEngine() {
-        verify(applicationContext).getBean(eq(FakeEngine.class));
-
         assertThat(engineFactory.getEngine(FakeEngine.TYPE)).isInstanceOf(FakeEngine.class);
     }
 }
