ba0e12d5ba616cc8e23c9554290077311f98a5da
Breno Queiroz <b.queiroz@entando.com>
2019-12-16 18:28:10 -0300

    PDA-49 Separating controller

--- src/main/java/org/entando/plugins/pda/controller/ProcessFormController.java
+++ src/main/java/org/entando/plugins/pda/controller/ProcessFormController.java
@@ -1,6 +1,7 @@
 package org.entando.plugins.pda.controller;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.entando.plugins.pda.controller.AuthPermissions.PROCESS_DEFINITION_FORM;
 import static org.entando.plugins.pda.controller.AuthPermissions.PROCESS_DEFINITION_LIST;
 import static org.entando.plugins.pda.controller.AuthPermissions.PROCESS_DIAGRAM;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -13,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.entando.plugins.pda.core.engine.Connection;
 import org.entando.plugins.pda.core.engine.Engine;
 import org.entando.plugins.pda.core.model.ProcessDefinition;
+import org.entando.plugins.pda.core.model.form.Form;
 import org.entando.plugins.pda.engine.EngineFactory;
 import org.entando.plugins.pda.service.ConnectionService;
 import org.entando.web.response.SimpleRestResponse;
@@ -27,34 +29,21 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "Process")
 @RequestMapping(path = "/connections/{connId}/processes")
 @RequiredArgsConstructor
-public class ProcessController {
+public class ProcessFormController {
 
     private final ConnectionService connectionService;
 
     private final EngineFactory engineFactory;
 
-    @Secured(PROCESS_DEFINITION_LIST)
-    @ApiOperation(notes = "Lists all processes definitions", nickname = "listProcessDefinitions",
-            value = "LIST ProcessDefinition")
-    @GetMapping(path = "/definitions", produces = APPLICATION_JSON_VALUE)
-    public SimpleRestResponse<List<ProcessDefinition>> listDefinitions(@PathVariable final String connId) {
-        log.info("Listing processes definitions for connection {}", connId);
+    @Secured(PROCESS_DEFINITION_FORM)
+    @ApiOperation(notes = "Get process form metadata", nickname = "getProcessForm", value = "GET ProcessForm")
+    @GetMapping(path = "/definitions/{id}/form", produces = {APPLICATION_JSON_VALUE})
+    public SimpleRestResponse<List<Form>> getProcessForm(@PathVariable String connId,
+            @PathVariable String id) {
+        log.info("Retrieving a process form definitions for connection {} and processId {}", connId, id);
         Connection connection = connectionService.get(connId);
         Engine engine = engineFactory.getEngine(connection.getEngine());
         return new SimpleRestResponse<>(
-                engine.getProcessService().listDefinitions(connection));
-    }
-
-    @Secured(PROCESS_DIAGRAM)
-    @ApiOperation(notes = "Gets the process diagram", nickname = "getProcessDiagram",
-            value = "GET Process Diagram")
-    @GetMapping(path = "/{id}/diagram", produces = "application/svg+xml")
-    public byte[] getProcessDiagram(@PathVariable final String connId,
-            @PathVariable final String id) {
-        log.info("Retrieving process diagram for {}", id);
-        Connection connection = connectionService.get(connId);
-        Engine engine = engineFactory.getEngine(connection.getEngine());
-        return engine.getProcessService().getProcessDiagram(connection, id)
-                .getBytes(UTF_8);
+                engine.getProcessFormService().getProcessForm(connection, id));
     }
 }
