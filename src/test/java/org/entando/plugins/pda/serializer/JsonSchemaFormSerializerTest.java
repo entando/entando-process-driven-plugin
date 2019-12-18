f1f6dd4d740b50f675802b5cad73409f8b9f8a98
ffleandro <filipeleandro87@gmail.com>
2019-12-18 15:21:01 -0300

    PDA-53 Process Form Json Schema (#20)
    * PDA-49 Adding controller for getting process form
    
    * PDA-52 created task comments apis;
    
    * PDA-53 implemented Json Schema Serializer to convert a Form into a Json Schema representation;
    
    * PDA-53 fixing issues from merge
    
    * PDA-53 fixing issues from merge
    
    * PDA-53 cleaning up
    
    * PDA-53 fixing PMD issue
    
    * PDA-53 fixing issues
    
    * PDA-53 fixing issues
    
    * PDA-53 fixing issues
    
    * PDA-53 fixing issues
    
    * PDA-53 removing unused class

---
+++ src/test/java/org/entando/plugins/pda/serializer/JsonSchemaFormSerializerTest.java
@@ -0,0 +1,34 @@
+package org.entando.plugins.pda.serializer;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.entando.plugins.pda.core.utils.TestUtils.readFromFile;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import org.entando.plugins.pda.core.service.process.FakeProcessFormService;
+import org.junit.Before;
+import org.junit.Test;
+
+public class JsonSchemaFormSerializerTest {
+
+    private ObjectMapper mapper;
+
+    @Before
+    public void setUp() {
+        mapper = new ObjectMapper();
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(new JsonSchemaFormSerializer());
+        mapper.registerModule(module);
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+    }
+
+    @Test
+    public void shouldSerializeProcessFormIntoJsonSchemaV7() throws Exception {
+        String result = mapper.writeValueAsString(
+                new V7JsonSchemaForm(FakeProcessFormService.FORMS));
+        String expected = readFromFile("process_form_json_schema_1.json");
+
+        assertThat(result).isEqualTo(expected);
+    }
+}
