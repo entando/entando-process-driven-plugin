f36435269dcb6e4651a49ee7f9bcb1c5837fd619
Jeff Go <j.go@entando.com>
2020-02-14 09:29:14 +0800

    PDA-62 Summary Card refactoring based on new Summary API specs (#42)
    * PDA-62 Summary Card - refactor based on new Summary API specs
    
    * PDA-62 summary card refactor final: interpolation for value formatting + removing actual config from summarycardconfig

--- widgets/src/components/SummaryCard/__tests__/SummaryCardValues.test.js
+++ widgets/src/components/SummaryCard/__tests__/SummaryCardValues.test.js
@@ -6,12 +6,10 @@ import 'mocks/i18nMock';
 describe('<SummaryCardValues />', () => {
   it('renders snapshot correctly', () => {
     const values = {
-      title: 'REQUESTS.TITLE',
-      totalLabel: 'REQUESTS.TOTAL_LABEL',
-      total: '2123',
-      percentage: '0.9243234',
+      value: 2123,
+      percentage: 0.9243234,
     };
-    const { container } = render(<SummaryCardValues values={values} />);
+    const { container } = render(<SummaryCardValues dataType="requests" values={values} />);
 
     expect(container).toMatchSnapshot();
   });
