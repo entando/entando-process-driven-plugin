83b5aae97d196cc90b1bcbd892ed9de5e3e5c006
Guilherme Rodrigues <guilherme.r89@gmail.com>
2020-02-17 11:58:11 -0300

    fixing unit tests

--- widgets/src/components/common/Table/custom/__tests__/ActionCell.test.js
+++ widgets/src/components/common/Table/custom/__tests__/ActionCell.test.js
@@ -2,18 +2,21 @@ import { render, fireEvent } from '@testing-library/react';
 import ActionCell from 'components/common/Table/custom/ActionCell';
 import 'mocks/i18nMock';
 
+const actions = {
+  openDiagram: jest.fn(),
+  selectTask: jest.fn(),
+};
+
 describe('<ActionCell />', () => {
   it('renders snapshot correctly', () => {
-    const { container } = render(
-      ActionCell({ showComplete: true, showClaim: true }, jest.fn())({})
-    );
+    const { container } = render(ActionCell({ showComplete: true, showClaim: true }, actions)({}));
 
     expect(container).toMatchSnapshot();
   });
 
   it('renders snapshot correctly without claim and complete button', () => {
     const { container } = render(
-      ActionCell({ showComplete: false, showClaim: false }, jest.fn())({})
+      ActionCell({ showComplete: false, showClaim: false }, actions)({})
     );
 
     expect(container).toMatchSnapshot();
@@ -21,7 +24,7 @@ describe('<ActionCell />', () => {
 
   it('shows Claim, Complete and Diagram buttons when button is clicked', () => {
     const { getByText, getByRole } = render(
-      ActionCell({ showComplete: true, showClaim: true }, jest.fn())({})
+      ActionCell({ showComplete: true, showClaim: true }, actions)({})
     );
 
     fireEvent.click(getByRole('button'));
