f01dbe1412ad3248b30456e8eb82b3b4b441f798
Jeff Go <j.go@entando.com>
2020-03-06 17:29:37 +0800

    PDA-149 dashboard overtime graph style change

--- widgets/src/components/OvertimeGraph/BarAreaChart.js
+++ widgets/src/components/OvertimeGraph/BarAreaChart.js
@@ -1,26 +1,81 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ComposedChart, XAxis, YAxis, Bar, ResponsiveContainer, Area, Legend } from 'recharts';
+import {
+  CartesianGrid,
+  ComposedChart,
+  XAxis,
+  YAxis,
+  Bar,
+  ResponsiveContainer,
+  Area,
+  Legend,
+} from 'recharts';
+import withStyles from '@material-ui/core/styles/withStyles';
 
-const BarAreaChart = ({ data, legends }) => (
-  <ResponsiveContainer>
-    <ComposedChart data={data}>
-      <XAxis dataKey="x" tickLine={false} />
-      <YAxis width={40} yAxisId="left" tickLine={false} />
-      <YAxis yAxisId="right" orientation="right" tickLine={false} />
-      <Bar yAxisId="left" dataKey="bar" barSize={20} fill="#1AB394" opacity={0.4} />
-      <Area yAxisId="right" dataKey="area" stroke="#464F88" fill="#464F88" fillOpacity={0.2} />
-      <Legend
-        iconType="square"
-        verticalAlign="top"
-        layout="vertical"
-        formatter={value => <span style={{ verticalAlign: 'middle' }}>{legends[value]}</span>}
-      />
-    </ComposedChart>
-  </ResponsiveContainer>
-);
+const styles = {
+  root: {
+    fontSize: 11,
+  },
+  legend: {
+    padding: 0,
+    listStyle: 'none',
+    '& li i': {
+      display: 'inline-block',
+      width: 10,
+      height: 10,
+      marginRight: 10,
+      verticalAlign: 'middle',
+    },
+  },
+};
+
+const BarAreaChart = ({ data, legends, classes }) => {
+  const CustomLegend = ({ payload }) => (
+    <ul className={classes.legend}>
+      {payload.map(entry => (
+        <li key={JSON.stringify(entry)}>
+          <i style={{ backgroundColor: entry.color }} />
+          {legends[entry.value]}
+        </li>
+      ))}
+    </ul>
+  );
+
+  CustomLegend.propTypes = {
+    payload: PropTypes.arrayOf({
+      color: PropTypes.string,
+      value: PropTypes.string,
+    }).isRequired,
+  };
+
+  return (
+    <ResponsiveContainer className={classes.root}>
+      <ComposedChart data={data}>
+        <XAxis dataKey="x" tickLine={false} padding={{ left: 10, right: 10 }} />
+        <YAxis yAxisId="left" tickLine={false} />
+        <YAxis yAxisId="right" orientation="right" tickLine={false} />
+        <CartesianGrid vertical={false} />
+        <Bar yAxisId="left" dataKey="bar" barSize={20} fill="#1AB394" opacity={0.4} />
+        <Area yAxisId="right" dataKey="area" stroke="#464F88" fill="#464F88" fillOpacity={0.2} />
+        <Legend
+          margin={{ top: -10 }}
+          layout="vertical"
+          height={1}
+          wrapperStyle={{ left: 105, top: 20 }}
+          content={CustomLegend}
+        />
+      </ComposedChart>
+    </ResponsiveContainer>
+  );
+};
+
+// formatter={value => <span style={{ verticalAlign: 'middle' }}>{legends[value]}</span>}
 
 BarAreaChart.propTypes = {
+  classes: PropTypes.shape({
+    root: PropTypes.string,
+    legend: PropTypes.string,
+  }).isRequired,
   data: PropTypes.arrayOf(
     PropTypes.shape({
       x: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
@@ -34,4 +89,4 @@ BarAreaChart.propTypes = {
   }).isRequired,
 };
 
-export default BarAreaChart;
+export default withStyles(styles, { name: 'BarAreaChart' })(BarAreaChart);
