20895a697931d80b4ac66cc11cfa36b356b4fafc
Rax Canaan W. Layumas <raxkaynan@gmail.com>
2020-03-10 19:57:05 +0800

    PDA-151 Add data fetching when TaskComments components updates (#67)

--- widgets/src/components/TaskComments/TaskCommentsContainer.js
+++ widgets/src/components/TaskComments/TaskCommentsContainer.js
@@ -41,6 +41,7 @@ class TaskCommentsContainer extends React.Component {
     this.closeNotification = this.closeNotification.bind(this);
     this.handleError = this.handleError.bind(this);
     this.fetchWidgetConfigs = this.fetchWidgetConfigs.bind(this);
+    this.fetchComments = this.fetchComments.bind(this);
     this.onClickAddComment = this.onClickAddComment.bind(this);
     this.onClickRemoveComment = this.onClickRemoveComment.bind(this);
   }
@@ -49,20 +50,16 @@ class TaskCommentsContainer extends React.Component {
     this.setState({ loading: true }, async () => {
       const fetchedConfig = await this.fetchWidgetConfigs();
 
-      this.setState({ config: fetchedConfig }, async () => {
-        const { config } = this.state;
-        const { taskId } = this.props;
-        const connection = (config && config.knowledgeSource) || '';
-
-        try {
-          const commentsResponse = await getTaskComments(connection, taskId);
-          this.setState({ comments: commentsResponse.payload || [], loading: false });
-        } catch (error) {
-          this.handleError(error.message);
-        }
-      });
+      this.setState({ config: fetchedConfig }, () => this.fetchComments());
     });
   }
+
+  componentDidUpdate = prevProps => {
+    const { taskId } = this.props;
+    if (prevProps.taskId !== taskId) {
+      this.fetchComments();
+    }
+  };
 
   onClickAddComment(comment) {
     this.setState({ addingComment: true }, async () => {
@@ -99,6 +96,19 @@ class TaskCommentsContainer extends React.Component {
   closeNotification = () => {
     this.setState({ errorMessage: '' });
   };
+
+  async fetchComments() {
+    const { config } = this.state;
+    const { taskId } = this.props;
+    const connection = (config && config.knowledgeSource) || '';
+
+    try {
+      const commentsResponse = await getTaskComments(connection, taskId);
+      this.setState({ comments: commentsResponse.payload || [], loading: false });
+    } catch (error) {
+      this.handleError(error.message);
+    }
+  }
 
   handleError(errorMessage) {
     this.setState({ errorMessage });
