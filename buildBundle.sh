e39b57848946a89d3fdcaa31c3f5062e61136362
Werbth Rocha <werbth@gmail.com>
2020-05-19 12:29:56 -0300

    PDA-244: Add config UI to ftl files

--- buildBundle.sh
+++ buildBundle.sh
@@ -4,14 +4,14 @@ set -e
 function injectResource() {
     local resource="$1"
     local destFile="$2"
+    local injectionPoint="$3"
 
-    local _NL=$'\\\n'
-
-    sed -i 's|'"$INJECTION_POINT"'|'"$resource$_NL$INJECTION_POINT"'|g' "$destFile"
+    sed -i 's|'"$injectionPoint"'|'"$resource$injectionPoint"'|g' "$destFile"
 }
 
 export -f injectResource
 export INJECTION_POINT="<#-- entando_resource_injection_point -->"
+export INJECTION_POINT_CONFIG="# entando_resource_injection_point"
 BUNDLE_NAME=$(awk -F':' 'NR==1 {gsub(/ /, "", $2); print $2}' ./bundle/descriptor.yaml)
 output_dir=bundle_out
 
@@ -43,7 +43,8 @@ for jspath in ${output_dir}/resources/static/js/*;
 do
     # This moves the referenced file to the top level ${output_dir}/resources/static dir for correct processing when loaded
     jsfile=$(basename "$jspath")
-    js_resources=${js_resources}"\n<script src=\"<@wp.resourceURL />${BUNDLE_NAME}/static/js/${jsfile}\"></script>"
+    config_ui_resources=${config_ui_resources}"    - ${BUNDLE_NAME}/static/js/${jsfile}\n"
+    js_resources=${js_resources}"<script src=\"<@wp.resourceURL />${BUNDLE_NAME}/static/js/${jsfile}\"></script>\n"
 done
 
 # CSS resources
@@ -51,14 +52,21 @@ for csspath in ${output_dir}/resources/static/css/*;
 do
     # This moves the referenced file to the top level ${output_dir}/resources/static dir for correct processing when loaded
     cssfile=$(basename "$csspath")
-
-    css_resources=${css_resources}"\n<link href=\"<@wp.resourceURL />${BUNDLE_NAME}/static/css/${cssfile}\" rel=\"stylesheet\">"
+    config_ui_resources=${config_ui_resources}"    - ${BUNDLE_NAME}/static/css/${cssfile}\n"
+    css_resources=${css_resources}"<link href=\"<@wp.resourceURL />${BUNDLE_NAME}/static/css/${cssfile}\" rel=\"stylesheet\">\n"
 done
 
-# Inject resources
-echo "- Injecting resources"
+# Inject resources on ftl files
+echo "- Injecting resources for FTL files"
 for ftlName in ${output_dir}/widgets/*.ftl;
 do
-    injectResource "$js_resources" "$ftlName"
-    injectResource "$css_resources" "$ftlName"
+    injectResource "$js_resources" "$ftlName" "$INJECTION_POINT"
+    injectResource "$css_resources" "$ftlName" "$INJECTION_POINT"
+done
+
+# Inject resources on descriptor files
+echo "- Injecting resources for config UI"
+for descriptorName in ${output_dir}/widgets/*.yaml;
+do
+    injectResource "$config_ui_resources" "$descriptorName" "$INJECTION_POINT_CONFIG"
 done
