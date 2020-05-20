63fab577d2fb25f39b39fba7f3e975b48f82de03
Werbth Rocha <werbth@gmail.com>
2020-05-20 13:51:47 -0300

    PDA-244: Fix inject resource and new line

--- buildBundle.sh
+++ buildBundle.sh
@@ -6,12 +6,14 @@ function injectResource() {
     local destFile="$2"
     local injectionPoint="$3"
 
-    sed -i 's|'"$injectionPoint"'|'"$resource$injectionPoint"'|g' "$destFile"
+    sed -i'.bak' 's|'"$injectionPoint"'|'"$resource$injectionPoint"'|g' "$destFile"
 }
 
 export -f injectResource
 export INJECTION_POINT="<#-- entando_resource_injection_point -->"
 export INJECTION_POINT_CONFIG="# entando_resource_injection_point"
+
+export _NL=$'\\\n'
 BUNDLE_NAME=$(awk -F':' 'NR==1 {gsub(/ /, "", $2); print $2}' ./bundle/descriptor.yaml)
 output_dir=bundle_out
 
@@ -43,8 +45,8 @@ for jspath in ${output_dir}/resources/static/js/*;
 do
     # This moves the referenced file to the top level ${output_dir}/resources/static dir for correct processing when loaded
     jsfile=$(basename "$jspath")
-    config_ui_resources=${config_ui_resources}"    - ${BUNDLE_NAME}/static/js/${jsfile}\n"
-    js_resources=${js_resources}"<script src=\"<@wp.resourceURL />${BUNDLE_NAME}/static/js/${jsfile}\"></script>\n"
+    config_ui_resources=${config_ui_resources}"    - ${BUNDLE_NAME}/static/js/${jsfile}$_NL"
+    js_resources=${js_resources}"<script src=\"<@wp.resourceURL />${BUNDLE_NAME}/static/js/${jsfile}\"></script>$_NL"
 done
 
 # CSS resources
@@ -52,8 +54,8 @@ for csspath in ${output_dir}/resources/static/css/*;
 do
     # This moves the referenced file to the top level ${output_dir}/resources/static dir for correct processing when loaded
     cssfile=$(basename "$csspath")
-    config_ui_resources=${config_ui_resources}"    - ${BUNDLE_NAME}/static/css/${cssfile}\n"
-    css_resources=${css_resources}"<link href=\"<@wp.resourceURL />${BUNDLE_NAME}/static/css/${cssfile}\" rel=\"stylesheet\">\n"
+    config_ui_resources=${config_ui_resources}"    - ${BUNDLE_NAME}/static/css/${cssfile}$_NL"
+    css_resources=${css_resources}"<link href=\"<@wp.resourceURL />${BUNDLE_NAME}/static/css/${cssfile}\" rel=\"stylesheet\">$_NL"
 done
 
 # Inject resources on ftl files
@@ -70,3 +72,7 @@ for descriptorName in ${output_dir}/widgets/*.yaml;
 do
     injectResource "$config_ui_resources" "$descriptorName" "$INJECTION_POINT_CONFIG"
 done
+
+# Remove backup files
+echo "- Remove backup files"
+rm ${output_dir}/widgets/*.bak;
