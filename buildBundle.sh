2c6bb6f62d82249ae9c8cc773c2c7550333883fe
Werbth Rocha <werbth@gmail.com>
2020-05-27 12:02:12 -0300

    PDA-244: PDA bundle (#123)
    * PDA-244: PDA bundle
    
    * PDA-244: Add config UI to ftl files
    
    * PDA-244: Fix configUi
    
    * adding descriptors and ftl files for all widgets (#124)
    
    * adding descriptors and ftl files for all widgets
    
    * removing unnecessary configUI from connections and auth widgets
    
    * removing auth widget
    
    * PDA-244: Add widgets to main descriptor
    
    * PDA-244: Fix inject resource and new line
    
    * moving folder structure and adding page models to the bundle (#125)
    
    * PDA-244: Fix build bundle and page models
    
    * add fragments to the bundle (#127)
    
    * PDA-244: Add fragments to the descriptor
    
    * PDA-244: Fix bundle
    
    * fixing keycloak auth (#128)
    
    Co-authored-by: Guilherme Rodrigues <guilherme.r89@gmail.com>

---
+++ buildBundle.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+set -e
+
+function injectResource() {
+    local resource="$1"
+    local destFile="$2"
+    local injectionPoint="$3"
+
+    sed -i'.bak' 's|'"$injectionPoint"'|'"$resource$injectionPoint"'|g' "$destFile"
+}
+
+export -f injectResource
+export INJECTION_POINT="<#-- entando_resource_injection_point -->"
+export INJECTION_POINT_CONFIG="# entando_resource_injection_point"
+
+export _NL=$'\\\n'
+BUNDLE_NAME=$(awk -F':' 'NR==1 {gsub(/ /, "", $2); print $2}' ./bundle/descriptor.yaml)
+output_dir=bundle_out
+
+echo "Generation of the $BUNDLE_NAME bundle"
+echo "====================================="
+echo ""
+
+## Build widgets
+echo "- Building widgets"
+cd widgets
+npm install
+npm run build
+cd ../
+
+# Clean previous build
+rm -rf ${output_dir}
+
+# Copy files to output directory
+echo "- Copying resources to ${output_dir}"
+mkdir -p ${output_dir}/resources/static/{js,css}
+cp -r widgets/build/static/js/*.js ${output_dir}/resources/static/js
+cp -r widgets/build/static/css/*.css ${output_dir}/resources/static/css
+cp -r bundle/* ${output_dir}/
+
+# JS resources
+for jspath in ${output_dir}/resources/static/js/*;
+do
+    # This moves the referenced file to the top level ${output_dir}/resources/static dir for correct processing when loaded
+    jsfile=$(basename "$jspath")
+    config_ui_resources=${config_ui_resources}"    - ${BUNDLE_NAME}/static/js/${jsfile}$_NL"
+    js_resources=${js_resources}"<script src=\"<@wp.resourceURL />${BUNDLE_NAME}/static/js/${jsfile}\"></script>$_NL"
+done
+
+# CSS resources
+for csspath in ${output_dir}/resources/static/css/*;
+do
+    # This moves the referenced file to the top level ${output_dir}/resources/static dir for correct processing when loaded
+    cssfile=$(basename "$csspath")
+    config_ui_resources=${config_ui_resources}"    - ${BUNDLE_NAME}/static/css/${cssfile}$_NL"
+    css_resources=${css_resources}"<link href=\"<@wp.resourceURL />${BUNDLE_NAME}/static/css/${cssfile}\" rel=\"stylesheet\">$_NL"
+done
+
+# Inject resources on ftl files
+echo "- Injecting resources for FTL files"
+for ftlName in ${output_dir}/widgets/*.ftl;
+do
+    injectResource "$js_resources" "$ftlName" "$INJECTION_POINT"
+    injectResource "$css_resources" "$ftlName" "$INJECTION_POINT"
+done
+
+# Inject resources on descriptor files
+echo "- Injecting resources for config UI"
+for descriptorName in ${output_dir}/widgets/*.yaml;
+do
+    injectResource "$config_ui_resources" "$descriptorName" "$INJECTION_POINT_CONFIG"
+done
+
+# Remove backup files
+echo "- Remove backup files"
+rm ${output_dir}/widgets/*.bak;
+
---
+++ bundle/fragments/entando_pda_import_react.ftl
@@ -0,0 +1,5 @@
+<#assign wp=JspTaglibs["/aps-core"]>
+<#assign c=JspTaglibs["http://java.sun.com/jsp/jstl/core"]>
+
+<script crossorigin src="https://unpkg.com/react@16/umd/react.production.min.js"></script>
+<script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>
