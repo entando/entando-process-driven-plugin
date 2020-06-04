6f9769b9bce615f071cdfd1c410e7af1f9d8fa63
Werbth Rocha <werbth@gmail.com>
2020-06-04 00:33:01 -0300

    PDA-234: Save authenticated user as initiator

--- src/main/java/org/entando/plugins/pda/controller/process/ProcessFormController.java
+++ src/main/java/org/entando/plugins/pda/controller/process/ProcessFormController.java
@@ -9,6 +9,7 @@ import io.swagger.annotations.ApiOperation;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.entando.keycloak.security.AuthenticatedUser;
 import org.entando.plugins.pda.core.engine.Connection;
 import org.entando.plugins.pda.core.engine.Engine;
 import org.entando.plugins.pda.engine.EngineFactory;
@@ -42,20 +43,18 @@ public class ProcessFormController {
         log.info("Retrieving a process form definitions for connection {} and processId {}", connId, id);
         Connection connection = connectionService.get(connId);
         Engine engine = engineFactory.getEngine(connection.getEngine());
-        return new V7JsonSchemaForm(
-                engine.getProcessFormService().get(connection, id));
+        return new V7JsonSchemaForm(engine.getProcessFormService().get(connection, id));
     }
 
     @Secured(PROCESS_DEFINITION_FORM_SUBMIT)
     @ApiOperation(notes = "Starts a new process", nickname = "submitProcessForm", value = "SUBMIT ProcessForm")
     @PostMapping(produces = {APPLICATION_JSON_VALUE})
     public SimpleRestResponse<String> submitProcessForm(@PathVariable String connId, @PathVariable String id,
-            @RequestBody Map<String, Object> form) {
+            @RequestBody Map<String, Object> form, AuthenticatedUser user) {
         log.info("Starting a new process for connection {} and id {}", connId, id);
 
         Connection connection = connectionService.get(connId);
         Engine engine = engineFactory.getEngine(connection.getEngine());
-        return new SimpleRestResponse<>(engine.getProcessFormService()
-                .submit(connection, id, form));
+        return new SimpleRestResponse<>(engine.getProcessFormService().submit(connection, id, form, user));
     }
 }
